150cf573f6192f1aac4da5c8cdc7ca5aecde7907
Nilay Patel <nilaypatel2@gmail.com>
2025-11-02 19:11:29 -0800

    Revert "Manually copy over libPyle.so to venv directory to ensure it's kept"
    This reverts commit daf928906a261a54c4c1f32b3287cdabd7d8d3b6.

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,9 @@ open System Lake DSL
 
 package pyle
 
+require "mathlib" 
+  from git "https://github.com/leanprover-community/mathlib4" @ "v4.24.0"
+
 @[default_target]
 lean_lib Pyle where
   defaultFacets := #[LeanLib.sharedFacet]
