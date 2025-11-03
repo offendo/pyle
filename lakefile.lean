c65bfc2ace333a6d18b0186630ae00dee07b1f03
Nilay Patel <nilaypatel2@gmail.com>
2025-11-02 21:06:41 -0800

    Static linking

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,6 @@ package pyle
 
 @[default_target]
 lean_lib Pyle where
-  defaultFacets := #[LeanLib.sharedFacet]
+  defaultFacets := #[LeanLib.staticExportFacet]
   buildType := Lake.BuildType.release
-  moreLeancArgs := #["-O3", "-Wl,-export_dynamic"]
+  moreLeancArgs := #["-O3"]
