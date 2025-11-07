400fd9b439beb6b2be95ac81f654a9600bbcf32b
Nilay Patel <nilaypatel2@gmail.com>
2025-11-07 12:00:33 -0800

    Cleanup - about to reset the repl dir

--- Pyle/Timeout.lean
+++ Pyle/Timeout.lean
@@ -14,37 +14,6 @@ def cancellableTimer (timeout : UInt32) : IO (Except IO.Error β) := do
         remaining := remaining - step
   throw <| IO.userError s!"error: lean server timeout after {timeout} milliseconds"
 
-
--- def runWithTimeout
---   (func : Unit → IO β)
---   (timeout : UInt32)
---   (prio : Task.Priority := Task.Priority.max) : IO (Except IO.Error β) :=
---   do
---     -- Launch a timer function to run in a separate thread
---     let timerFunc: IO (Except IO.Error β) := do
---         IO.sleep $ timeout
---         return Except.error $ IO.userError s!"error: lean server timeout after {timeout} milliseconds"
---     let timer <- IO.asTask timerFunc default -- make sure to use a separate thread, otherwise we get arbitrary delay
---
---     -- Launch the main task
---     let funcWrapper: IO (Except IO.Error β) := func () >>= fun b => return .ok b
---     let job <- IO.asTask funcWrapper prio
---
---     -- Wait for whichever finishes first
---     let result <- IO.waitAny [job, timer]
---
---     -- Cancel the timer manually if it's still going
---     if not (<- IO.hasFinished timer) then IO.cancel timer
---     if not (<- IO.hasFinished job) then IO.cancel job
---
---     -- Return the result
---     match result with
---       | Except.ok val => return val
---         -- TODO: Not sure how to throw error here properly; should basically never happen
---       | Except.error err => return (Except.error err.toString)
-
-
-
 def runWithTimeout
   (func : Unit → IO β)
   (timeout : UInt32)
--- leak.py
+++ leak.py
@@ -5,16 +5,31 @@ skyrocket by 300mb every iteration if you watch on a monitor.
 """
 
 import pyle
+import time
+import gc
 
-_, _, _, _, initial_state = pyle.evaluate("-- setup\n", initial_state=None, timeout=0)
+# start = time.time()
+# _, _, _, _, initial_state = pyle.evaluate("-- setup\n", initial_state=None, timeout=0)
+
+# # Reuse the state
+# for i in range(1000):
+#     msgs, trees, err, duration, state = pyle.evaluate(
+#         "def x : Nat := 5\nexample : x == 5 := by trivial\n#print x",
+#         initial_state=initial_state,
+#         timeout=1_000,
+#     )
+# end = time.time()
+# print(f'single: {end - start}')
+
+
+start = time.time()
+_, state_cache = pyle.evaluate_many(["import Mathlib\nimport Aesop\n-- setup"], None, timeout=0)
 
 # Reuse the state
-for i in range(100000):
-    print(i)
-    msgs, trees, err, time, state = pyle.evaluate(
-        "def x : Nat := 5\nexample : x == 5 := by trivial\n#print x",
-        initial_state=initial_state,
-        timeout=1_000,
-    )
-    print(msgs)
-    # Use same initial_state for next iteration
+outputs, state_cache = pyle.evaluate_many(
+    ["import Mathlib\nimport Aesop\ndef x : Real := 5\ndef y : Real := 6\nexample : x == 5 := by trivial\n#eval (x + y)"] * 1000,
+    state_cache=state_cache,
+    timeout=1_000,
+)
+end = time.time()
+print(f'batched: {end - start}')
